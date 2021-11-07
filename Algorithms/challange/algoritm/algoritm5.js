260df7857bcb3bd9e87e16e44558cc9e7e15ec90
Vesile Hesenova <vesiih01@gmail.com>
2021-11-08 01:51:08 +0400

    5ci algoritmi etdim ve coxx maraqli alindi

---
+++ Algorithms/challange/algoritm/algoritm5.js
@@ -0,0 +1,6 @@
+let rahatKodYazanlar = ["Rehman", "Elmar", "Sebuhi", "Ravi"];
+let kodYazmagaCalisanlar = ["Vesi", "Ilkin", "Fezail"];
+
+var SamirMuelliminTelebeleri = rahatKodYazanlar.concat(kodYazmagaCalisanlar);
+
+console.log(SamirMuelliminTelebeleri)
