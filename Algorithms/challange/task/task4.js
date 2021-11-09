f9dbba13f0e2f54ff8fca144cbd31d48e8089ed6
Vesile Hesenova <vesiih01@gmail.com>
2021-11-10 01:24:43 +0400

    4cu taski etdim

---
+++ Algorithms/challange/task/task4.js
@@ -0,0 +1,32 @@
+let x = 0
+
+
+
+function artir() {
+    x++
+    document.getElementById("number").innerHTML = x
+
+
+
+
+}
+
+
+function reset() {
+    x = 0
+    document.getElementById("number").innerHTML = x
+
+
+
+
+
+}
+
+
+function azalt() {
+    x--
+    document.getElementById("number").innerHTML = x
+
+
+
+}
