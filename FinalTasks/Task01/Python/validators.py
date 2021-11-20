e5e32026539ca9aeef9fdd5ba24018e59aad962b
Vesile Hesenova <vesiih01@gmail.com>
2021-11-20 19:20:25 +0400

    ferqli metoddan istifade etdim, arasdirma ederek algoritmi hell etdim

---
+++ FinalTasks/Task01/Python/validators.py
@@ -0,0 +1,8 @@
+#sozu daxil edib yoxlamaq: herflerin elifba sirasi ile duzulusunu, sadece reqemlerden ibaret olmasini, sadece kicik herflerden ibaret olmasini ve sadece boyuk herflerden ibaret olmasini
+
+x=input('sozu daxil edin :')
+print(x.isalnum())  #alfasayisal
+print(x.isalpha())  #elifba sirasi
+print(x.isdigit())  #reqem
+print(x.islower())  #kicik herfler
+print(x.isupper())  #boyuk herfler
