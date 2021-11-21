4988ffda9ce272efce26b745723d3f971f642bdc
Vesile Hesenova <vesiih01@gmail.com>
2021-11-22 02:48:50 +0400

    hackerrankda alinmadi ama visualda netice verir dee(

---
+++ FinalTasks/Task01/Python/lists_hck.py
@@ -0,0 +1,13 @@
+N=int(input())
+list=[N]
+list.insert(0,5)
+list.insert(1,10)
+list.insert(0,6)
+print(list)
+list.remove(6)
+list.append(9)
+list.append(1)
+list.sort()
+list.pop(0)
+list.reverse()
+print(list)
