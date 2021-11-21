31101a31e231d79efc8e0bf2fdd684bf11909c24
Vesile Hesenova <vesiih01@gmail.com>
2021-11-22 02:51:19 +0400

    daxil edilen ad ve soyad metnin icine elave olunur

---
+++ FinalTasks/Task01/Python/name_hck.py
@@ -0,0 +1,10 @@
+#ad ve soyad daxil etmek lazimdi ve cap edende "salam (ad,soyad)! siz sadece olaraq Python ile tanish oldunuz" sozu yazilmalidir
+
+x=str(input('Adinizi daxil edin :'))
+y=str(input('Soyadinizi daxil edin:'))
+txt= "Salam {} {}! Siz sadece olaraq Python ile tanish oldunuz"
+print(txt.format(x,y))
+
+
+
+
