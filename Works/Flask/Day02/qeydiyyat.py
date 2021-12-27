7c4e329a15821aad6f44992ab8353cc132bc99d0
Vesile Hesenova <vesiih01@gmail.com>
2021-12-27 23:18:03 +0400

    .

--- Works/Flask/Day02/qeydiyyat.py
+++ Works/Flask/Day02/qeydiyyat.py
@@ -1,6 +1,6 @@
 
 
-class qeydiyyat():
+class Qeydiyyat():
     def __init__(self,ad,soyad,email,telefon, shifre):
         self.ad = ad
         self.soyad = soyad
@@ -9,28 +9,28 @@ class qeydiyyat():
         self.shifre = shifre
 
     def ad(self):
-        self.ad=input(str("adinizi daxil edin: "))    
+        self.ad=input("adinizi daxil edin: ")    
         if self.ad== "":
             return self.ad
         
 
     def soyad(self):
-        self.soyad=input(str("soyadinizi daxil edin: "))
+        self.soyad=input("soyadinizi daxil edin: ")
         if self.soyad=="":
             return self.soyad
 
     def email(self):
-        self.email=input(str("emailinizi daxil edin: "))
+        self.email=input("emailinizi daxil edin: ")
 
     def telefon(self):
-        self.telefon=input(str("telefon nomrenizi daxil edin: +994 " ))    
+        self.telefon=input("telefon nomrenizi daxil edin: +994 " )    
         if len(self.telefon)!=9:
             print("telefon nomresinin uzunlugu 9 olmalidir") 
         else:
             print("nomreni duzgun daxil edin")    
 
     def shifre(self):
-        self.shifre=input(str("shifrenizi daxil edin: "))
+        self.shifre=input("shifrenizi daxil edin: ")
         if len(self.shifre)<8:
             print("en azi 8 xarakter daxil etmelisiniz")
         if len(self.shifre)>15:
@@ -38,9 +38,5 @@ class qeydiyyat():
 
 
 
-goster=qeydiyyat()
+goster=Qeydiyyat('vesi','hesenova','vesi@mail.com','05522222222','236458')
 goster.ad()
-goster.soyad()
-goster.email()
-goster.telefon()
-goster.shifre()
